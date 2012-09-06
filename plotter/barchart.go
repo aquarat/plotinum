36f68d28b7617d6ccfc7f70d626842d0e5724c88
Ethan Burns <burns.ethan@gmail.com>
2012-09-06 16:21:03 -0400

    Merged with default.

--- plotter/barchart.go
+++ plotter/barchart.go
@@ -8,10 +8,11 @@ import (
 	"code.google.com/p/plotinum/plot"
 	"code.google.com/p/plotinum/vg"
 	"image/color"
+	"math"
 )
 
 type BarChart struct {
-	XYs
+	Values
 
 	// Width is the width of the bars.
 	Width vg.Length
@@ -26,20 +27,16 @@ type BarChart struct {
 	// When the Offset is zero, the bars are drawn
 	// centered at their x location.
 	Offset vg.Length
-}
 
-// NewBarChartXY returns a new bar chart with
-// a single bar for each XY value.  The height of
-// the bar is the Y value, and the x location
-// corresponds to the X value.
-func NewBarChartXY(xys XYer, width vg.Length) *BarChart {
-	return &BarChart{
-		XYs:       CopyXYs(xys),
-		Width:     width,
-		Color:     color.Black,
-		LineStyle: DefaultLineStyle,
-	}
+	// XMin is the X location of the first bar.  XMin
+	// can be changed to move groups of bars
+	// down the X axis in order to make grouped
+	// bar charts.
+	XMin float64
 
+	// stackedOn is the bar chart upon which
+	// this bar chart is stacked.
+	stackedOn *BarChart
 }
 
 // NewBarChart returns a new bar chart with
@@ -48,30 +45,55 @@ func NewBarChartXY(xys XYer, width vg.Length) *BarChart {
 // correspond to the index of their value in the
 // Valuer.
 func NewBarChart(vs Valuer, width vg.Length) *BarChart {
-	return NewBarChartXY(indexXs{vs}, width)
+	return &BarChart{
+		Values:    CopyValues(vs),
+		Width:     width,
+		Color:     color.Black,
+		LineStyle: DefaultLineStyle,
+	}
 }
 
-// indexXs implements the XYer interface, returning the
-// indices for the x values and the values for the y values.
-type indexXs struct{ Valuer }
+// BarHeight returns the maximum y value of the
+// ith bar, taking into account any bars upon
+// which it is stacked.
+func (b *BarChart) BarHeight(i int) float64 {
+	ht := 0.0
+	if b == nil {
+		return 0
+	}
+	if i >= 0 && i < len(b.Values) {
+		ht += b.Values[i]
+	}
+	if b.stackedOn != nil {
+		ht += b.stackedOn.BarHeight(i)
+	}
+	return ht
+}
 
-func (v indexXs) XY(i int) (float64, float64) {
-	return float64(i), v.Value(i)
+// StackOn stacks a bar chart on top of another,
+// and sets the XMin and Offset to that of the
+// chart upon which it is being stacked.
+func (b *BarChart) StackOn(on *BarChart) {
+	b.XMin = on.XMin
+	b.Offset = on.Offset
+	b.stackedOn = on
 }
 
 // Plot implements the plot.Plotter interface.
 func (b *BarChart) Plot(da plot.DrawArea, plt *plot.Plot) {
 	trX, trY := plt.Transforms(&da)
 
-	for _, xy := range b.XYs {
-		xmin := trX(xy.X)
+	for i, ht := range b.Values {
+		x := b.XMin + float64(i)
+		xmin := trX(float64(x))
 		if !da.ContainsX(xmin) {
 			continue
 		}
 		xmin = xmin - b.Width/2 + b.Offset
 		xmax := xmin + b.Width
-		ymin := trY(0)
-		ymax := trY(xy.Y)
+		bottom := b.stackedOn.BarHeight(i)
+		ymin := trY(bottom)
+		ymax := trY(bottom + ht)
 
 		pts := []plot.Point{
 			{xmin, ymin},
@@ -90,15 +112,26 @@ func (b *BarChart) Plot(da plot.DrawArea, plt *plot.Plot) {
 
 // DataRange implements the plot.DataRanger interface.
 func (b *BarChart) DataRange() (xmin, xmax, ymin, ymax float64) {
-	xmin, xmax, _, ymax = XYRange(b)
+	xmin = b.XMin
+	xmax = xmin + float64(len(b.Values)-1)
+
+	ymin = math.Inf(1)
+	ymax = math.Inf(-1)
+	for i, y := range b.Values {
+		ybot := b.stackedOn.BarHeight(i)
+		ytop := ybot + y
+		ymin = math.Min(ymin, math.Min(ybot, ytop))
+		ymax = math.Max(ymax, math.Max(ybot, ytop))
+	}
 	return
 }
 
 // GlyphBoxes implements the GlyphBoxer interface.
 func (b *BarChart) GlyphBoxes(plt *plot.Plot) []plot.GlyphBox {
-	boxes := make([]plot.GlyphBox, len(b.XYs))
-	for i, xy := range b.XYs {
-		boxes[i].X = plt.X.Norm(xy.X)
+	boxes := make([]plot.GlyphBox, len(b.Values))
+	for i := range b.Values {
+		x := b.XMin + float64(i)
+		boxes[i].X = plt.X.Norm(x)
 		boxes[i].Rect = plot.Rect{
 			Min:  plot.Point{X: b.Offset - b.Width/2},
 			Size: plot.Point{X: b.Width},
